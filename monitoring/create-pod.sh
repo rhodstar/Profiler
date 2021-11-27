368b614a5942dff8f35ab88e7ddbd8e88910872f
rhodstar <rhodfra@gmail.com>
2021-11-26 19:44:43 -0600

    Added working script for pod generation

--- monitoring/create-pod.sh
+++ monitoring/create-pod.sh
@@ -5,10 +5,11 @@ podman network create mon-net
 echo "Creating monitoring pod"
 podman pod create --name=monitoring 
 
-#echo "Creating cadvisor"
-#podman run --name=monitoring_cadvisor_1 -it -d -p 8080:8080 --pod=monitoring --net mon-net  --mount type=bind,source=/,destination=/rootfs,ro --mount type=bind,source=/var/run,destination=/var/run,ro --mount type=bind,source=/sys,destination=/sys,ro --mount type=bind,source=/var/lib/docker/,destination=/var/lib/docker,ro --mount type=bind,source=/dev/disk,destination=/dev/disk,ro gcr.io/cadvisor/cadvisor 
 echo "Creating redis"
 podman run --name=monitoring_redis_1 -it -d -p 6379:6379 --pod=monitoring --net mon-net redis:latest
+#echo "Creating cadvisor"
+#podman run --name=monitoring_cadvisor_1 -it -d -p 8080:8080 --pod=monitoring --net mon-net --mount type=bind,source=/,destination=/rootfs,ro --mount type=bind,source=/var/run,destination=/var/run,ro --mount type=bind,source=/sys,destination=/sys,ro --mount type=bind,source=/var/lib/containers/,destination=/var/lib/containers,ro --mount type=bind,source=/dev/disk,destination=/dev/disk,ro gcr.io/cadvisor/cadvisor 
+#podman run -it -d --pod=monitoring --net mon-net --volume=/:/rootfs:ro --volume=/var/run:/var/run:ro --volume=/sys:/sys:ro --volume=/var/lib/containers/:/var/lib/containers:ro --volume=/dev/disk/:/dev/disk:ro --publish=8080:8080 --name=monitoring_cadvisor_1 --privileged --device=/dev/kmsg gcr.io/cadvisor/cadvisor
 echo "Creating prometheus"
 podman run --name=monitoring_prometheus_1 -it -d -p 9090:9090 --pod=monitoring --net mon-net --mount type=bind,source=/f/dev/Profiler/monitoring/./prometheus.yml,destination=/etc/prometheus/prometheus.yml,ro prom/prometheus:latest --config.file=/etc/prometheus/prometheus.yml
 echo "Creating grafana"
--- monitoring/delete-pod.sh
+++ monitoring/delete-pod.sh
@@ -1,6 +1,5 @@
 echo "Doing some cleaning"
 podman pod stop monitoring
 podman rm $(podman container ls -aq)
-podman rmi gcr.io/cadvisor/cadvisor
 podman pod rm monitoring
 podman network rm mon-net
